acddad6946e78435bdf59789f949fccfd98b2a84
Deshan Vimukthi <vimudeshan123@gmail.com>
2021-06-10 23:54:03 +0530

    Upload the project

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import SideBar from './components/Sidebar';
+
+
+function App() {
+  return (
+    <div className="App">
+
+        <SideBar />
+       
+    </div>
+  );
+}
+
+export default App;
