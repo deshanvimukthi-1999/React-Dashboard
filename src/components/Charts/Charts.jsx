d27793aff4ec095ff753176357fb1db0954093e1
Deshan Vimukthi <vimudeshan123@gmail.com>
2021-06-12 17:31:28 +0530

    Edit table

--- src/components/Charts/Charts.jsx
+++ src/components/Charts/Charts.jsx
@@ -1,12 +1,12 @@
 import "./charts.css";
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import {TableBody, TableCell ,TableContainer,TableHead ,Paper  }from '@material-ui/core';
+import {Table,TableBody, TableCell ,TableContainer,TableHead ,TableRow,Paper  }from '@material-ui/core';
 
 
     const useStyles = makeStyles({
         table: {
-          minWidth: 200,
+          minWidth: 500,
         },
       });
 
@@ -15,64 +15,51 @@ import {TableBody, TableCell ,TableContainer,TableHead ,Paper  }from '@material-
       }
 
       const rows = [
-        createData('Frozen yoghurt', 159, 6.04),
-        createData('Ice cream', 237, 9.0),
-        createData('Eclair', 262, 16.0, 24),
-        createData('Eclair', 262, 16.0, 24),
-        createData('Eclair', 262, 16.0, 24),
+        createData('Pizza', 159, 6.04),
+        createData('French Fries', 237, 9.0),
+        createData('Multigrain Hot Cereal', 262, 16.0, 24),
+        createData('Friend Egg Sandwich', 262, 16.0, 24),
       ];
 
       export default function Charts() {
-     
+        const classes = useStyles();
       
             return (
-                <div className="chart">
-                <TableContainer className="headerTitle">
-                    <h5>RECENT ORDERS REQUESTED</h5>
-                    <button className="btn1">View All</button>
-                    <hr />
-                    <TableHead className="tHead">
-                        <TableCell align="right">FoodItems</TableCell>
-                        <TableCell align="right"> Price</TableCell>
-                        <TableCell align="right">ProductID </TableCell>
-                    </TableHead>
-
-                    <TableBody>
-                        <TableCell align="right">
-                            <img className="image" src="http://slidesigma.nyc/templatemonster/react/foodtech/assets/img/foodtech/pizza.jpg" alt="imge" />
-                            Pizza
-                        </TableCell>
-                        <TableCell align="right">ada</TableCell>
-                        <TableCell align="right">adas</TableCell>
-                    </TableBody>
-
-                    <TableBody>
-                        <TableCell align="right">
-                            <img className="image" src="http://slidesigma.nyc/templatemonster/react/foodtech/assets/img/foodtech/french-fries.jpg" alt="imge1" />
-                            faf
-                        </TableCell>
-                        <TableCell align="right">ada</TableCell>
-                        <TableCell align="right">adas</TableCell>
-                    </TableBody>
-
-                    <TableBody>
-                        <TableCell align="right">
-                            <img className="image" src="http://slidesigma.nyc/templatemonster/react/foodtech/assets/img/foodtech/cereals.jpg" alt="imge2" />
-                            faf
-                        </TableCell>
-                        <TableCell align="right">ada</TableCell>
-                        <TableCell align="right">adas</TableCell>
-                    </TableBody>
-
-                    <TableBody>
-                        <TableCell align="right">
-                            <img className="image" src="http://slidesigma.nyc/templatemonster/react/foodtech/assets/img/foodtech/egg-sandwich.jpg" alt="imge3" />
-                            faf
-                        </TableCell>
-                        <TableCell align="right">ada</TableCell>
-                        <TableCell align="right">adas</TableCell>
-                    </TableBody>
-                </TableContainer>
+                <div className="container1">
+                <div className="tableHeader">RECENT ORDERS REQUESTED</div>
+                <div className="buttonClass">
+                <button className="button">View All</button>
                 </div>
-            );
-    }
+                <Table striped bordered hover>
+                    <thead>
+                        <tr>
+                        <th>#</th>
+                        <th>First Name</th>
+                        <th>Last Name</th>
+                        <th>Username</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                        <td>1</td>
+                        <td>Mark</td>
+                        <td>Otto</td>
+                        <td>@mdo</td>
+                        </tr>
+                        <tr>
+                        <td>2</td>
+                        <td>Jacob</td>
+                        <td>Thornton</td>
+                        <td>@fat</td>
+                        </tr>
+                        <tr>
+                        <td>3</td>
+                        <td>Thornton</td>
+                        <td>@fat</td>
+                        <td>@twitter</td>
+                        </tr>
+                    </tbody>
+                </Table>
+        </div>
+        );
+}
