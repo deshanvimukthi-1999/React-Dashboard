1da8f67d977d66df4aa2e25d63844a540b8a76f2
Deshan Vimukthi <vimudeshan123@gmail.com>
2021-06-13 05:57:42 +0530

    Update the code

--- src/components/Charts/Charts.jsx
+++ src/components/Charts/Charts.jsx
@@ -1,65 +1,53 @@
-import "./charts.css";
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import {Table,TableBody, TableCell ,TableContainer,TableHead ,TableRow,Paper  }from '@material-ui/core';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+import "./charts.css";
+
+function createData(FoodItem, Price, ProductID) {
+  return { FoodItem, Price, ProductID };
+}
+
+const rows = [
+  createData('Pizza', 159, 6.0),
+  createData('French Fries', 237, 9.03),
+  createData('Multigrain Hot Cerea', 262, 16.0,),
+  createData('Fried Egg Sandwich', 305, 3.7),
+
+];
+
+export default function Charts() {
 
 
-    const useStyles = makeStyles({
-        table: {
-          minWidth: 500,
-        },
-      });
-
-      function createData(FoodItems, Price, ProductID) {
-        return {FoodItems, Price, ProductID  };
-      }
-
-      const rows = [
-        createData('Pizza', 159, 6.04),
-        createData('French Fries', 237, 9.0),
-        createData('Multigrain Hot Cereal', 262, 16.0, 24),
-        createData('Friend Egg Sandwich', 262, 16.0, 24),
-      ];
-
-      export default function Charts() {
-        const classes = useStyles();
-      
-            return (
-                <div className="container1">
-                <div className="tableHeader">RECENT ORDERS REQUESTED</div>
-                <div className="buttonClass">
+  return (
+    <TableContainer component={Paper}>
+        <div className="container1">
+            <div className="tableHeader">RECENT ORDERS REQUESTED</div>
+              <div className="buttonClass">
                 <button className="button">View All</button>
                 </div>
-                <Table striped bordered hover>
-                    <thead>
-                        <tr>
-                        <th>#</th>
-                        <th>First Name</th>
-                        <th>Last Name</th>
-                        <th>Username</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                        <td>1</td>
-                        <td>Mark</td>
-                        <td>Otto</td>
-                        <td>@mdo</td>
-                        </tr>
-                        <tr>
-                        <td>2</td>
-                        <td>Jacob</td>
-                        <td>Thornton</td>
-                        <td>@fat</td>
-                        </tr>
-                        <tr>
-                        <td>3</td>
-                        <td>Thornton</td>
-                        <td>@fat</td>
-                        <td>@twitter</td>
-                        </tr>
-                    </tbody>
-                </Table>
-        </div>
-        );
+        <TableHead>
+          <TableRow>
+            <TableCell>Food Item</TableCell>
+            <TableCell align="right"> Price</TableCell>
+            <TableCell align="right">Product ID</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <TableRow key={row.FoodItem}>
+              <TableCell component="th" scope="row">
+                {row.FoodItem}
+              </TableCell>
+              <TableCell align="right">{row.Price}</TableCell>
+              <TableCell align="right">{row.ProductID}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </div>
+    </TableContainer>
+  );
 }
