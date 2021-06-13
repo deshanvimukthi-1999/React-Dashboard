ba67874f026107510328325066c77138ae61f023
Deshan Vimukthi <vimudeshan123@gmail.com>
2021-06-13 14:45:16 +0530

    Adding Tables and Chart

--- src/components/Pages/Home.jsx
+++ src/components/Pages/Home.jsx
@@ -1,13 +1,20 @@
-import Charts from "../Charts/Charts";
+import Chart from "../Chart/Chart";
 import Feature from "../Infofeachers/Feature";
 import "./home.css";
+import { userData } from "../../dummyData";
+import WidgetLg from "../../components/widgetLg/WidgetLg";
+import TableFood from "../tableFood/TableFood";
 
 
 export default function Home() {
     return (
         <div className="home">
            <Feature />
-           <Charts />
+           <Chart data={userData} title="User Analytics" grid dataKey="Active User"/>
+           <div className="homeWidgets">
+                <TableFood />
+                <WidgetLg />
+           </div>
         </div>
     )
 }
---
+++ src/components/tableFood/TableFood.jsx
@@ -0,0 +1,89 @@
+import "./tableFood.css";
+import { Visibility } from "@material-ui/icons";
+
+export default function TableFood() {
+  return (
+    <div className="tableFood">
+      <span className="tableFoodTitle">RECENT ORDERS REQUESTED</span>
+      <hr />
+      <ul className="tableFoodList">
+        <li className="tableFoodListItem">
+          <img
+            src="http://slidesigma.nyc/templatemonster/react/foodtech/assets/img/foodtech/pizza.jpg"
+            alt="pizza"
+            className="tableFoodImg"
+          />
+          <div className="tableFoodUser">
+            <span className="tableFoodUsername">Pizza</span>
+            <span className="tableFoodUserTitle">$19.99</span>
+           
+          </div>
+          <button className="tableFoodButton">
+            <Visibility className="tableFoodIcon" />
+          View All
+          </button>
+        </li>
+        <li className="tableFoodListItem">
+          <img
+            src="http://slidesigma.nyc/templatemonster/react/foodtech/assets/img/foodtech/french-fries.jpg"
+            alt="fries"
+            className="tableFoodImg"
+          />
+          <div className="tableFoodUser">
+            <span className="tableFoodUsername">French Fries</span>
+            <span className="tableFoodUserTitle">	$14.59</span>
+          </div>
+          <button className="tableFoodButton">
+            <Visibility className="tableFoodIcon" />
+          View All
+          </button>
+        </li>
+        <li className="tableFoodListItem">
+          <img
+            src="http://slidesigma.nyc/templatemonster/react/foodtech/assets/img/foodtech/cereals.jpg"
+            alt="cereals"
+            className="tableFoodImg"
+          />
+          <div className="tableFoodUser">
+            <span className="tableFoodUsername"> Multigrain Hot Cereal</span>
+            <span className="tableFoodUserTitle">$25.22</span>
+          </div>
+          <button className="tableFoodButton">
+            <Visibility className="tableFoodIcon" />
+          View All
+          </button>
+        </li>
+        <li className="tableFoodListItem">
+          <img
+            src="http://slidesigma.nyc/templatemonster/react/foodtech/assets/img/foodtech/egg-sandwich.jpg"
+            alt="eggSalad"
+            className="tableFoodImg"
+          />
+          <div className="tableFoodUser">
+            <span className="tableFoodUsername"> Fried Egg Sandwich</span>
+            <span className="tableFoodUserTitle">$11.23</span>
+          </div>
+          <button className="tableFoodButton">
+            <Visibility className="tableFoodIcon" />
+          View All
+          </button>
+        </li>
+        <li className="tableFoodListItem">
+          <img
+            src="https://img.traveltriangle.com/blog/wp-content/uploads/2018/12/cover-for-street-food-in-sydney.jpg"
+            alt="burger"
+            className="tableFoodImg"
+          />
+          <div className="tableFoodUser">
+            <span className="tableFoodUsername">Burgers</span>
+            <span className="tableFoodUserTitle">$8.23</span>
+          </div>
+          <button className="tableFoodButton">
+            <Visibility className="tableFoodIcon" />
+          View All
+          </button>
+        </li>
+      </ul>
+    </div>
+  );
+}
