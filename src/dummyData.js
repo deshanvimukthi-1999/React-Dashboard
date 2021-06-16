0e9b9a9b6691da493aed8a48763670062430c8d7
Deshan Vimukthi <vimudeshan123@gmail.com>
2021-06-16 05:50:06 +0530

    update my code

--- src/dummyData.js
+++ src/dummyData.js
@@ -49,18 +49,3 @@ export const userData = [
     },
   ];
 
-
-  export const foodData = [
-    {
-      name: "Jan",
-      "Sales": 4000,
-    },
-    {
-      name: "Feb",
-      "Sales": 3000,
-    },
-    {
-      name: "Mar",
-      "Sales": 5000,
-    },
-  ];
